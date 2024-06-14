fe6b9e8a896d338b1360d4110af80e1b3eaf1c38
valensyah <valen99830@gmail.com>
2024-06-15 02:15:15 +0700

    update halaman home page dengan tampilan movie dari api moviedb

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const axios = require('axios')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,7 +36,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const test = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=0fbe3dc01cd368614ceb64fe4ba06e32')
+  // console.log(test.data)
+  const movies = test.data.results
+  res.render('index', { title: 'Express', movies });
 });
 
 module.exports = router;
