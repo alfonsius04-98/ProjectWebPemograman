6c5f86fed8d3bf49049a64a7d64db4eb8d86ebdb
valensyah <valen99830@gmail.com>
2024-06-15 11:23:19 +0700

    update frontend : pelatihan sesi pertama penambahan validasi input dan modal history pembelian beserta integrasi api public movie dan install axios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const movies = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=2c6dfda27285e553ec344b86b3f6c92d')
+  res.render('index', { title: 'Express', data: movies.data.results });
 });
 
 module.exports = router;
